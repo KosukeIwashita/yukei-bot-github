20a8b72af4ba4d7d87c3a00e0b3a7e57a42103cc
KosukeIwashita <70099027+KosukeIwashita@users.noreply.github.com>
2020-08-27 22:03:21 +0900

    Update line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -1,64 +1,62 @@
 from flask import Flask, request, abort
-
 from linebot import (
-   LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-   InvalidSignatureError
+    InvalidSignatureError
 )
 from linebot.models import (
-   MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
 )
-
 import os
-import requests
+#追加
+import pya3rt
 
 app = Flask(__name__)
 
-TALKAPI_KEY = os.environ['tk_ap']
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["line_channel_token"]
 YOUR_CHANNEL_SECRET = os.environ["line_channel_secret"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-def talkapi(text):
-   url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
-   req = requests.post(url, {'apikey':TALKAPI_KEY,'query':text}, timeout=5)
-   data = req.json()
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
-   if data['status'] != 0:
-      return data['message']
-
-   msg = data['results'][0]['reply']
-   return msg
 
 @app.route("/callback", methods=['POST'])
 def callback():
-   # get X-Line-Signature header value
-   signature = request.headers['X-Line-Signature']
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-   # get request body as text
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+    return 'OK'
 
-   # handle webhook body
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       print("Invalid signature. Please check your channel access token/channel secret.")
-       abort(400)
 
-   return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   push_text = event.message.text
-   msg = talkapi(push_text)
-   line_bot_api.reply_message(
-       event.reply_token,
-       TextSendMessage(text=msg))
+    #（追加）talk_aiメソッドに引数を渡して返り値をai_messageに代入
+    ai_message = talk_ai(event.message.text)
+    line_bot_api.reply_message(
+        event.reply_token,
+        #TextSendMessage(text=event.message.txt))
+        #（修正）ai_messageを返すようにする
+        TextSendMessage(text=ai_message))
+
+#（追加）pya3rtでai会話を返信
+def talk_ai(word):
+    apikey = os.environ["tk_ap"]
+    client = pya3rt.TalkClient(apikey)
+    reply_message = client.talk(word)
+    return reply_message['results'][0]['reply']
 
 if __name__ == "__main__":
-   port = int(os.getenv("PORT"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
