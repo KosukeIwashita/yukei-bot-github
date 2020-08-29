59a934cb0440410c02b04610e5277c0a57f1c558
KosukeIwashita <70099027+KosukeIwashita@users.noreply.github.com>
2020-08-29 20:57:10 +0900

    Update yukei-bot.py

--- yukei-bot/yukei-bot.py
+++ yukei-bot/yukei-bot.py
@@ -1,50 +1,69 @@
-from flask import Flask,request,abort
-from linebot import LineBotApi,WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent,TextMessage,TextSendMessage
+from flask import Flask, request, abort
+
+from linebot import (
+   LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+   InvalidSignatureError
+)
+from linebot.models import (
+   MessageEvent, TextMessage, TextSendMessage,
+)
+
 import os
 import requests
-import pprint
 import pya3rt
 
-app=Flask(__name__)
-#環境変数の取得
-YOUR_CHANNEL_ACCESS_TOKEN="l93wr01BmymtABAEVZxmWQ/QEl/ipfVjemoA4s6ClYQZ1SpTJIBQaddiMMM4bixbs94c6DSOOv+2CaOpF6tjDKqK/JtB7JhphBrXLK8z43Xjl7IMdmQGAVTnxHz9k+Utd9kgUw5eli6+F1LJUQSqaAdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET="2bd122c701510001c9bd7d68c536a1c5"
-line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+app = Flask(__name__)
 
-@app.route("/callback",methods=["POST"])
+TALKAPI_KEY = os.environ['tk_ap']
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["line_channel_token"]
+YOUR_CHANNEL_SECRET = os.environ["line_channel_secret"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+def talkapi(text):
+   url = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk'
+   req = requests.post(url, {'apikey':TALKAPI_KEY,'query':text}, timeout=5)
+   data = req.json()
+
+   if data['status'] != 0:
+      return data['message']
+
+   msg = data['results'][0]['reply']
+   return msg
+
+@app.route("/")
+def hello_world():
+   return "hello world!"
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature=request.headers["X-Line-Signature"]
+   # get X-Line-Signature header value
+   signature = request.headers['X-Line-Signature']
 
-    body=request.get_data(as_text=True)
-    app.logger.info("Request body"+body)
+   # get request body as text
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
 
-    try:
-        handler.handle(body,signature)
-    except InvalidSignatureError:
-        abort(400)
-    return "OK"
+   # handle webhook body
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       print("Invalid signature. Please check your channel access token/channel secret.")
+       abort(400)
 
-@handler.add(MessageEvent,message=TextMessage)
+   return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #入力された文字列を格納
-    push_text = event.message.text
+   push_text = event.message.text
+   msg = talkapi(push_text)
+   line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=msg))
 
-    #A3RTのTalkAPIにより応答
-    reply_text = talkapi_response(push_text)
-
-    #リプライ部分の記述
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
-
-#A3RTのTalkAPIにより応答
-def talkapi_response(text):
-    apikey = "DZZSPWtxfG7VGhTzbvsSqYzP3FJvQrUP"
-    client = pya3rt.TalkClient(apikey)
-    response = client.talk(text)
-    return ((response['results'])[0])['reply']
-
-if __name__=="__main__":
-    port=int(os.getenv("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+if __name__ == "__main__":
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
